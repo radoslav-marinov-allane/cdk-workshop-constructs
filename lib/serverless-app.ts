088e0e4894786823c52ad5ece18737fefb1d6a34
unknown <radoslav.marinov.extern@allane.com>
2023-11-23 12:40:39 +0200

    Abstracting construct for web app

--- lib/serverless-app.ts
+++ lib/serverless-app.ts
@@ -1,23 +1,31 @@
 import { IConstruct } from "constructs";
 
-import * as origins    from "aws-cdk-lib/aws-cloudfront-origins";
-import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
+import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3_deployment from "aws-cdk-lib/aws-s3-deployment";
-import * as s3            from "aws-cdk-lib/aws-s3";
-
 import * as cdk from "aws-cdk-lib";
+
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as wafv2 from "aws-cdk-lib/aws-wafv2";
+import * as fs from "fs";
+
 
 /**
  * Use following Interface to define all Properties that your ServerlessApp requires!
  */
 export interface ServerlessAppProps extends cdk.StackProps {
-  localPathToApiCode: string;
-  localPathToAppCode: string;
-  // ...
+  tableName: string
+  functionHandler: string
+  functionPath: string
+  functionName: string
+  openApiPath: string
+  webAppPath: string
 }
 
 export class ServerlessApp extends cdk.Stack {
@@ -34,5 +42,150 @@ export class ServerlessApp extends cdk.Stack {
     // ### S3 && CDN
 
     // ...
+
+
+    // Dynamo
+    let myTable = new dynamodb.Table(this, props.tableName, {
+      tableName: props.tableName,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        type: dynamodb.AttributeType.STRING,
+        name: "id",
+      },
+    });
+
+    // λ-Func
+    // let role = new iam.Role(this, "MyRole", {
+    //   assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    // });
+
+    // role.addManagedPolicy(
+    //   iam.ManagedPolicy.fromAwsManagedPolicyName(
+    //     "service-role/AWSLambdaBasicExecutionRole"
+    //   )
+    // );
+
+    let fn = new lambda.Function(this, "MyFunc", {
+      functionName: props.functionName,
+      architecture: lambda.Architecture.ARM_64,
+      timeout: cdk.Duration.seconds(10),
+      runtime: lambda.Runtime.PYTHON_3_11,
+      handler: props.functionHandler,
+      code: lambda.Code.fromAsset(
+        props.functionPath
+      ),
+      // TODO: Parameterize <REGION>
+      layers: [
+        lambda.LayerVersion.fromLayerVersionArn(
+          this,
+          "foo",
+          `arn:aws:lambda:${cdk.Stack.of(this).region
+          }:017000801446:layer:AWSLambdaPowertoolsPythonV2-Arm64:22`
+        ),
+      ],
+      environment: { TABLE: myTable.tableName },
+    });
+    myTable.grantReadWriteData(fn);
+
+
+    let fnUrl = fn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    // API-Gateway
+    let raw = fs
+      .readFileSync(props.openApiPath)
+      .toString();
+    raw = raw.replace(
+      /{uri}/g,
+      `arn:aws:apigateway:${cdk.Stack.of(this).region
+      }:lambda:path/2015-03-31/functions/arn:aws:lambda:${cdk.Stack.of(this).region
+      }:${cdk.Stack.of(this).account}:function:${fn.functionName}/invocations`
+    );
+
+    let api = new apigateway.SpecRestApi(this, "rest-api", {
+      apiDefinition: apigateway.ApiDefinition.fromInline(JSON.parse(raw)),
+    });
+
+    fn.addPermission("execution-permissions", {
+      principal: new iam.ServicePrincipal("apigateway.amazonaws.com"),
+      sourceArn: api.arnForExecuteApi("*"),
+    });
+
+    // WAF
+    let waf = new wafv2.CfnWebACL(this, "waf", {
+      defaultAction: { allow: {} },
+      scope: "REGIONAL",
+      visibilityConfig: {
+        cloudWatchMetricsEnabled: true,
+        sampledRequestsEnabled: true,
+        metricName: "waf-vehicle-service",
+      },
+      rules: [
+        {
+          name: "rate-limit",
+          priority: 1,
+          action: { block: {} },
+          statement: {
+            rateBasedStatement: {
+              aggregateKeyType: "IP",
+              limit: 100,
+            },
+          },
+          visibilityConfig: {
+            sampledRequestsEnabled: true,
+            cloudWatchMetricsEnabled: true,
+            metricName: "waf-rate-limit",
+          },
+        },
+        {
+          name: "geo-match",
+          priority: 2,
+          action: { block: {} },
+          statement: {
+            geoMatchStatement: {
+              countryCodes: ["US"],
+            },
+          },
+          visibilityConfig: {
+            sampledRequestsEnabled: true,
+            cloudWatchMetricsEnabled: true,
+            metricName: "waf-geo-match",
+          },
+        },
+      ],
+    });
+
+    const resourceArn = `arn:${cdk.Aws.PARTITION}:apigateway:${cdk.Aws.REGION}::/restapis/${api.restApiId}/stages/${api.deploymentStage.stageName}`;
+
+    new wafv2.CfnWebACLAssociation(this, "waf-assoc", {
+      resourceArn: resourceArn,
+      webAclArn: waf.attrArn,
+    });
+
+    // S3
+    let website = new s3.Bucket(this, "website");
+
+    new s3_deployment.BucketDeployment(this, "deployment", {
+      destinationBucket: website,
+      sources: [
+        s3_deployment.Source.asset(
+          props.webAppPath
+        ),
+      ],
+    });
+
+    // CDN
+    const oai = new cloudfront.OriginAccessIdentity(this, "oai");
+    website.grantRead(oai);
+
+    new cloudfront.Distribution(this, "distribution", {
+      defaultRootObject: "index.html",
+      defaultBehavior: {
+        origin: new origins.S3Origin(website, {
+          originAccessIdentity: oai,
+        }),
+      },
+    });
   }
 }
